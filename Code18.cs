c5309820953391ea5582820784ff7f56a05ad6e7
ZlobnyKarlik <79357067+ZlobnyKarlik@users.noreply.github.com>
2024-03-20 16:26:13 +0300

    Add files via upload

---
+++ Code18.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Checkbox_Click(object sender, RoutedEventArgs e)
+        {
+            if (CB.IsChecked == true) GB.IsEnabled = true;
+
+        }
+        private void Checkbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (CB.IsChecked == false) GB.IsEnabled = false;   
+        }   
+        private void rb1_Checked(object sender, RoutedEventArgs e)
+        {
+            tb.Text = "История, Иностр.Яз.";
+        }
+        private void rb2_Checked(object sender, RoutedEventArgs e)
+        {
+            tb.Text = "Культурология,История,Иностр.Яз.";
+        }
+        private void CB1_Click(object sender, RoutedEventArgs e)
+        {
+            if (CB1_IsChecked == true)  CB1_IsEnabled = true; 
+        }
+
+    }
+}
